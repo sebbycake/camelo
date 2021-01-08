ee46db195048583c2e8ad814c7de2b4b41e396be
sebastian <leehur_sebastian@hotmail.com>
2021-01-08 12:39:33 +0800

    Added more storylines and challenges, and created a new storyline.css to store all storyline classes

--- js/data.js
+++ js/data.js
@@ -2,6 +2,36 @@
 
 const tasksArray = [
     {
+      storyline: "On 25th December 2020, Cathy the Camel is born into the Flaming Desert.",
+      challenge: "Make baby Cathy appear in the Flaming Desert!",
+      property: "Display",
+      hint: "Note CSS Syntax: selector {property:value}Use the display property to make Cathy appear!.",
+    },
+    {
+      storyline: "Cathy the Camel opened her eyes for the first time to the beautiful Flaming Desert.",
+      challenge: "Add color to the Desert for baby Cathy!",
+      property: "Background-color",
+      hint: "Use the background-color property to change its color!",
+    },
+    {
+      storyline: "It’s to teach Cathy the Camel a few tricks!",
+      challenge: "Make baby Cathy walk a few steps to the right.",
+      property: "Padding/Margin",
+      hint:"Try using the padding or margin properties to move Cathy!",
+    },
+    {
+      storyline: "Flaming Desert is known to be a dangerous place. Before letting Cathy the Camel explore into the woods, let's teach her some survival skills.",
+      challenge: "Help baby Cathy build a shelter that can protect her from the wild.",
+      property: "border",
+      hint: "Try using the border property!"
+    }, 
+    {
+      storyline: "3 years later... Wow how time flies! Cathy the Camel has finally grown up. To our delight, with the abundance of resources, Cathy has indeed grown to be little more huge than expected",
+      challenge: "Show the grown-up Cathy!",
+      property: "width and height",
+      hint: "Play around with the width and height property!"
+    },
+    {
         storyline: "Cathy the Camel has been walking along Flaming Desert for the past four hours...",
         challenge: "<strong>Challenge 1: </strong>Bring Cathy the Camel to the puddle of water to quench its thirst!",
         property: "In this challenge, we will be using Flexbox properties.",
@@ -30,7 +60,7 @@ const tasksArray = [
 
 const graphicsArray = [
     {
-        className: 'water',
+        className: 'puddle',
         imgFile: 'puddle.png',
     },
     {
